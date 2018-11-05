7c3a889c32d475d189d55a1117269878d96d804b
Alexander Behrmann <alexander.behrmann@hotmail.com>
2018-11-05 15:47:04 -0800

    Backend tools setup

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # currentSea
+
+# How to start the backend server
+
+Just type in the terminal when you are in the current-sea-back folder "npm start".
+You can then use your browser by going to http://localhost:4000/ and checking that you see the test message.
+
+I have also set up some tools to help us with development and consistency. You can check out all the packages I installed in package.json in the current-sea-back folder.
+
+Happy coding! :-)
+
+A.
--- current-sea-back/app.js
+++ current-sea-back/app.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const chalk = require('chalk');
 const mysql = require('mysql');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
+const port = process.env.PORT;
 
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/static/'))); // Serve static files from static folder
+
+
+app.get('/', (req, res) => {
+  res.send('Test message');
+});
+
+// Database configuration DO NOT MODIFY!
 const config = {
   host: 'currentsea-mysqldb.cewrfkrguurr.us-west-1.rds.amazonaws.com',
   user: 'admin',
@@ -15,5 +29,9 @@ const dbConnection = mysql.createConnection(config);
 
 dbConnection.connect((err) => {
   if (err) { throw err; }
-  console.log('Connection established');
+  debug('Connection established with MySQL');
+});
+
+app.listen(port, () => {
+  debug(`Listening on port ${chalk.green(port)}`);
 });
