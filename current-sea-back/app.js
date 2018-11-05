ab0edc37ba0128865962e7855d5ca60f9336387e
Alexander Behrmann <alexander.behrmann@hotmail.com>
2018-11-05 09:16:11 -0800

    db connection established

--- current-sea-back/app.js
+++ current-sea-back/app.js
@@ -2,3 +2,18 @@ const express = require('express');
 const mysql = require('mysql');
 
 const app = express();
+
+const config = {
+  host: 'currentsea-mysqldb.cewrfkrguurr.us-west-1.rds.amazonaws.com',
+  user: 'admin',
+  password: 'CurrentSea',
+  port: 3306,
+  database: 'currency_db',
+};
+
+const dbConnection = mysql.createConnection(config);
+
+dbConnection.connect((err) => {
+  if (err) { throw err; }
+  console.log('Connection established');
+});
