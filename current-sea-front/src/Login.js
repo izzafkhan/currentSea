1a143a54bd5e3b4981795a2d8232d4df9cfa0a61
izzafkhan <izzafkhan@gmail.com>
2018-11-13 13:30:17 -0800

    Add files via upload
    Skeletons of login/register pages

--- current-sea-front/src/Login.js
+++ current-sea-front/src/Login.js
@@ -1,38 +1,31 @@
 import React, { Component } from 'react';
-import Header from './Header.js';
-import './Login.css';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import RaisedButton from 'material-ui/RaisedButton';
-import TextField from 'material-ui/TextField';
+import Header from '../Header.js';
+import LoginForm from '../Login/LoginForm.js';
+import SignupForm from '../Login/SignupForm.js';
+
+import { Link } from "react-router-dom";
+
 
 
 export default class Login extends Component{
+    constructor(props){
+        super(props);
+    }
+
   render() {
       return(
+
         <div>
-          <Header/>
-            <MuiThemeProvider>
-                <div>
-                    <h1 className="login">Login</h1>
-                    <TextField loginTxt="Enter login email"
-                    floatingLabelText= "Email"
-                    />
-                    <br/>
-                    <TextField 
-                        type="password"
-                        hintText="Enter password"
-                        
-                        />
-                        <br/>
-                   <RaisedButton label ="Enter" primary={true} floatingLabelText="Password" style={style} />
-                </div>
-            </MuiThemeProvider>
+     
+            <Header/>
+            <LoginForm/>
+            
+            
         </div>
+  
+       
     );
   }
 };
 
-const style={
-    margin:15,textAlign:'center'
-    
-};
+
