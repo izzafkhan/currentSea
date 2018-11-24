304c6b10a4c6b2bfceed4ecea1f8866fea44c648
Danny <dannyfung97@gmail.com>
2018-11-24 00:09:58 -0800

    Update bkAccountRoutes.js

--- current-sea-back/routes/bkAccountRoutes.js
+++ current-sea-back/routes/bkAccountRoutes.js
@@ -5,84 +5,100 @@ const db = require('./db');
 const bkAccountRouter = express.Router();
 
 module.exports = function router() {
-  bkAccountRouter.use((err, req, res) => {
-    if (!req.user) {
-      res.status(401).send('User does not appear to exist.');
-    }
-  });
-
   bkAccountRouter.route('/add_account')
     .post((req, res) => {
-      const {
-        userId, accountName, accountId,
-      } = req.body;
-      db.query('SELECT at_account_name from account_table where at_user_id = ? and at_account_name = ? and at_account_id = ?', [userId, accountName, accountId], (err, result) => {
-        if (result.length === 0) {
-          if (accountId === '' || accountName === '') {
-            res.status(401).json({ message: 'Please fill in the blank spaces of the parameters.' });
-          } else {
-            db.query('INSERT INTO account_table(at_account_name, at_account_id, at_user_id) VALUES (?, ?, ?);', [accountName, accountId, userId], (results) => {
-              if (err) {
-                throw err;
+      if (req.user) {
+        const {
+          accountName, accountId,
+        } = req.body;
+        db.query('SELECT at_account_name from account_table where at_user_id = ? and at_account_name = ? and at_account_id = ?',
+          [req.user.username, accountName, accountId], (err, result, fields) => {
+            if (err) {
+              debug(err);
+              res.status(500).json({ message: 'Some error occurred' });
+            }
+            if (result.length === 0) {
+              if (accountId === '' || accountName === '') {
+                res.status(401).json({ message: 'Please fill in the blank spaces of the parameters.' });
+              } else {
+                db.query('INSERT INTO account_table(at_account_name, at_account_id, at_user_id) VALUES (?, ?, ?);', [accountName, accountId, req.user.username],
+                  (err2, results, fields) => {
+                    if (err2) {
+                      debug(err2);
+                      res.status(500).json({ message: 'Some error occurred' });
+                    }
+                    res.status(201).json({ message: 'Account created' });
+                  });
               }
-              debug(results);
-            });
-          }
-        } else {
-          res.status(401).json({ message: 'Account already exists.' });
-        }
-      });
+            } else {
+              res.status(401).json({ message: 'Account already exists.' });
+            }
+          });
+      } else {
+        res.status(401).json({ message: 'User is not logged in' });
+      }
     });
 
   bkAccountRouter.route('/edit_account')
-    .post((req, res) => {
-      const {
-        userId, accountId, accountName, newAccountId, newAccountName,
-      } = req.body;
-      db.query('SELECT account_name from account_table where at_user_id = ? AND at_account_name = ? AND at_account_id = ?', [userId, accountName, accountId], (err, results) => {
-        if (results.length !== 0) {
-          if (newAccountId !== accountId || newAccountName !== accountName) {
-            if (newAccountId !== accountId) {
-              db.query(('UPDATE account_table SET at_account_id = ? WHERE at_user_id = ? AND at_account_id = ?', [newAccountId, userId, accountId], () => {
-                if (err) {
-                  throw err;
-                }
-                debug(results);
-              }));
+    .put((req, res) => {
+      if (req.user) {
+        const {
+          accountId, accountName, newAccountId, newAccountName,
+        } = req.body;
+        db.query('SELECT account_name from account_table where at_user_id = ? AND at_account_name = ? AND at_account_id = ?', [req.user.username, accountName, accountId], (err, results) => {
+          if (results.length !== 0) {
+            if (newAccountId !== accountId || newAccountName !== accountName) {
+              if (newAccountId !== accountId) {
+                db.query(('UPDATE account_table SET at_account_id = ? WHERE at_user_id = ? AND at_account_id = ?', [newAccountId, req.user.username, accountId], () => {
+                  if (err) {
+                    debug(err);
+                    res.status(500).json({ message: 'Some error occurred' });
+                  }
+                  debug(results);
+                }));
+              }
+              if (newAccountName !== accountName) {
+                db.query(('UPDATE account_table SET at_account_name = ? WHERE at_user_id = ? AND at_account_name = ?', [newAccountName, req.user.username, accountName], () => {
+                  if (err) {
+                    debug(err);
+                    res.status(500).json({ message: 'Some error occurred' });
+                  }
+                  debug(results);
+                }));
+              }
+            } else if (newAccountId === '' || newAccountName === '') {
+              res.status(401).json({ message: 'Attempted changes cannot be blank.' });
+            } else {
+              res.status(401).json({ message: 'Account name and id are not changed.' });
             }
-            if (newAccountName !== accountName) {
-              db.query(('UPDATE account_table SET at_account_name = ? WHERE at_user_id = ? AND at_account_name = ?', [newAccountName, userId, accountName], () => {
-                if (err) {
-                  throw err;
-                }
-                debug(results);
-              }));
-            }
-          } else if (newAccountId === '' || newAccountName === '') {
-            res.status(401).json({ message: 'Attempted changes cannot be blank.' });
           } else {
-            res.status(401).json({ message: 'Account name and id are not changed.' });
+            res.status(401).json({ message: 'Account not found.' });
           }
-        } else {
-          res.status(401).json({ message: 'Account not found.' });
-        }
-      });
+        });
+      } else {
+        res.status(401).json({ message: 'User is not logged in' });
+      }
     });
 
   bkAccountRouter.route('/delete_account')
-    .post((req, res) => {
-      const { userId, accountId, accountName } = req.body;
-      db.query('SELECT account_name from account_table where at_user_id = ? AND at_account_name = ? OR at_account_id = ?', [userId, accountName, accountId], (err, results) => {
-        if (results.length !== 0) {
-          db.query('DELETE FROM account_table WHERE at_user_id = ? AND at_account_name = ? OR at_account_id = ?', [userId, accountName, accountId], () => {
-            if (err) {
-              throw err;
-            }
-          });
-        } else {
-          res.status(401).json({ message: 'Account not found.' });
-        }
-      });
+    .delete((req, res) => {
+      if (req.user) {
+        const { userId, accountId, accountName } = req.body;
+        db.query('SELECT account_name from account_table where at_user_id = ? AND at_account_name = ? OR at_account_id = ?', [userId, accountName, accountId], (err, results) => {
+          if (results.length !== 0) {
+            db.query('DELETE FROM account_table WHERE at_user_id = ? AND at_account_name = ? OR at_account_id = ?', [userId, accountName, accountId], () => {
+              if (err) {
+                debug(err);
+                res.status(500).json({ message: 'Some error occurred' });
+              }
+            });
+          } else {
+            res.status(401).json({ message: 'Account not found.' });
+          }
+        });
+      } else {
+        res.status(401).json({ message: 'User is not logged in' });
+      }
     });
   return bkAccountRouter;
 };
