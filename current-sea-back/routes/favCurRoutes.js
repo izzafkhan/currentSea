e8a401eba426f7359d67c97f0c4a53013aa12fc1
Danny <dannyfung97@gmail.com>
2018-11-24 20:30:01 -0800

    edited routes

--- current-sea-back/routes/favCurRoutes.js
+++ current-sea-back/routes/favCurRoutes.js
@@ -1,29 +1,99 @@
 const express = require('express');
+const debug = require('debug')('app:favCurRoutes');
 const db = require('./db');
 
 const favCurRouter = express.Router();
 
 module.exports = function router() {
-  favCurRouter.use((err, req, res) => {
-    if (!req.user) {
-      res.status(401).send('User does not appear to exist.');
-    }
+  favCurRouter.route('/currencies').get((req, res) => {
+    db.query('SELECT DISTINCT ct_from FROM currency_table', (err, results, fields) => {
+      if (err) {
+        debug(err);
+        res.status(500).json({ message: 'Some error occurred' });
+      } else {
+        const output = [];
+        for (let i = 0; i < results.length; i += 1) {
+          output[i] = results[i].ct_from;
+        }
+        res.status(200).json({ currencies: output });
+      }
+    });
   });
 
   favCurRouter.route('/add_fav_curr')
     .post((req, res) => {
-      const { userId, currencyAbv } = req.body;
-      db.query('SELECT * FROM Favorite_Currency_table WHERE fc_user_id = ? AND fc_currency_abv = ?', [userId, currencyAbv], (results, err) => {
-        if (results.length !== 0) {
-          db.query('INSERT INTO Favorite_Currency_table (fc_user_id, fc_currency_abv, fc_change_amount) VALUES (?, ?, ?);', [userId, currencyAbv, 0], () => {
+      if (req.user) {
+        const { currencyFrom, currencyTo } = req.body;
+        db.query('SELECT * FROM favorite_currency_table WHERE ft_user_id = ? AND ft_from = ? AND ft_to = ?',
+          [req.user.username, currencyFrom, currencyTo], (err, results, fields) => {
             if (err) {
-              throw err;
+              debug(err);
+              res.status(500).json({ message: 'Some error occurred' });
+            }
+            debug(results);
+            if (results.length === 0) {
+              db.query('INSERT INTO favorite_currency_table (ft_user_id, ft_from, ft_to, ft_change_in_amount) VALUES (?, ?, ?, ?);',
+                [req.user.username, currencyFrom, currencyTo, 0], (err2, results2, fields2) => {
+                  if (err2) {
+                    debug(err2);
+                    res.status(500).json({ message: 'Some error occurred' });
+                  } else {
+                    res.status(201).json({ message: 'Currencies inserted successfully' });
+                  }
+                });
+            } else {
+              res.status(401).json({ message: 'Currency already favorited.' });
             }
           });
+      } else {
+        res.status(401).json({ message: 'User is not logged in' });
+      }
+    });
+
+  favCurRouter.route('/remove_fav_cur')
+    .post((req, res) => {
+      if (req.user) {
+        const { currencyFrom, currencyTo } = req.body;
+        db.query('SELECT * FROM favorite_currency_table WHERE ft_user_id = ? AND ft_from = ? AND ft_to = ?',
+          [req.user.username, currencyFrom, currencyTo], (err, results, fields) => {
+            if (err) {
+              debug(err);
+              res.status(500).json({ message: 'Some error occurred' });
+            }
+            debug(results);
+            if (results.length === 0) {
+              res.status(401).json({ message: 'Currency already unfavorited.' });
+            } else {
+              db.query('DELETE FROM favorite_currency_table WHERE ft_user_id = ? AND ft_from = ? AND ft_to = ?',
+                [req.user.username, currencyFrom, currencyTo], (err2, results2, fields2) => {
+                  if (err2) {
+                    debug(err2);
+                    res.status(500).json({ message: 'Some error occurred' });
+                  } else {
+                    res.status(201).json({ message: 'Currencies inserted successfully' });
+                  }
+                });
+            }
+          });
+      } else {
+        res.status(401).json({ message: 'User is not logged in' });
+      }
+    });
+
+  favCurRouter.route('/getrate').get((req, res) => {
+    const { from, to, date } = req.query;
+    db.query('SELECT ct_rate FROM currency_table WHERE ct_from = ? AND ct_to = ? AND ct_date = ?',
+      [from, to, date], (err, results, fields) => {
+        if (err) {
+          debug(err);
+          res.status(500).json({ message: 'Some error occurred' });
+        }
+        if (results.length > 0) {
+          res.status(200).json({ rate: results[0].ct_rate });
         } else {
-          res.status(401).json({ message: 'Currency already favorited.' });
+          res.status(200).json({ message: 'No rates for these currencies and this day' });
         }
       });
-    });
+  });
   return favCurRouter;
 };
--- current-sea-back/routes/transactionsRoutes.js
+++ current-sea-back/routes/transactionsRoutes.js
@@ -25,57 +25,56 @@ module.exports = function router() {
           });
       } else {
         res.status(401).json({ message: 'User is not logged in' });
+      }
+    });
 
-      };
-
-      transactionsRouter.route('/edit_transactions')
-        .put((req, res) => {
-          if (req.user) {
-            const {
-              transactionId, userId, accountId, date, eventId, debitAmt, creditAmt, currencyId,
-            } = req.body;
-            db.query('SELECT * FROM tt_transaction_table WHERE tt_transaction_id = ?', [transactionId], (results) => {
-              if (results.length === 0) {
-                res.status(401).json({ message: 'Transaction id does not exist.' });
-              } else {
-                db.query('UPDATE tt_transaction_table SET ',
-                  'tt_user_id = ?, tt_account_id = ?, tt_date = ?, tt_event_id = ?, ',
-                  'tt_debit_amount = ?, tt_credit_amount = ?, tt_currency_abv = ? WHERE tt_transaction_id = ?;',
-                  [userId, accountId, date, eventId, debitAmt, creditAmt, currencyId, transactionId], (err) => {
-                    if (err) {
-                      throw err;
-                    }
-                    debug(results);
-                  });
-              }
-            });
+  transactionsRouter.route('/edit_transactions')
+    .put((req, res) => {
+      if (req.user) {
+        const {
+          transactionId, userId, accountId, date, eventId, debitAmt, creditAmt, currencyId,
+        } = req.body;
+        db.query('SELECT * FROM tt_transaction_table WHERE tt_transaction_id = ?', [transactionId], (results) => {
+          if (results.length === 0) {
+            res.status(401).json({ message: 'Transaction id does not exist.' });
           } else {
-            res.status(401).json({ message: 'User is not logged in' });
+            db.query('UPDATE tt_transaction_table SET ',
+              'tt_user_id = ?, tt_account_id = ?, tt_date = ?, tt_event_id = ?, ',
+              'tt_debit_amount = ?, tt_credit_amount = ?, tt_currency_abv = ? WHERE tt_transaction_id = ?;',
+              [userId, accountId, date, eventId, debitAmt, creditAmt, currencyId, transactionId], (err) => {
+                if (err) {
+                  throw err;
+                }
+                debug(results);
+              });
           }
         });
+      } else {
+        res.status(401).json({ message: 'User is not logged in' });
+      }
+    });
 
-      transactionsRouter.route('/delete_transactions')
-        .delete((req, res) => {
-          if (req.user) {
-            const { transactionId } = req.body;
-            db.query('SELECT tt_transaction_id from transaction_table where tt_transaction_id = ?', [transactionId], (err, result) => {
-              if (result.length === 0) {
-                res.status(401).json({ message: 'Transaction id does not exist.' });
-              } else {
-                db.query('DELETE FROM tt_transaction_table WHERE tt_transaction_id = ?;',
-                  [transactionId], (results) => {
-                    if (err) {
-                      throw err;
-                    }
-                    debug(results);
-                  });
-              }
-            });
+  transactionsRouter.route('/delete_transactions')
+    .delete((req, res) => {
+      if (req.user) {
+        const { transactionId } = req.body;
+        db.query('SELECT tt_transaction_id from transaction_table where tt_transaction_id = ?', [transactionId], (err, result) => {
+          if (result.length === 0) {
+            res.status(401).json({ message: 'Transaction id does not exist.' });
           } else {
-            res.status(401).json({ message: 'User is not logged in' });
-
+            db.query('DELETE FROM tt_transaction_table WHERE tt_transaction_id = ?;',
+              [transactionId], (results) => {
+                if (err) {
+                  throw err;
+                }
+                debug(results);
+              });
           }
         });
-      return transactionsRouter;
-    }
+      } else {
+        res.status(401).json({ message: 'User is not logged in' });
 
+      }
+    });
+  return transactionsRouter;
+};
