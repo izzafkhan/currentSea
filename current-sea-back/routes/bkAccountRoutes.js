99b3073fc342722329593037a3424064f45660dd
saramofrad <smofrad@ucsd.edu>
2018-11-25 15:10:19 -0800

    trying to fix editaccount route

--- current-sea-back/routes/bkAccountRoutes.js
+++ current-sea-back/routes/bkAccountRoutes.js
@@ -19,12 +19,12 @@ module.exports = function router() {
         accountName, accountId,
       } = req.body;
       db.query('SELECT at_account_name from account_table where at_user_id = ? and at_account_name = ? and at_account_id = ?',
-        [req.user.username, accountName, accountId], (err, result, fields) => {
+        [req.user.username, accountName, accountId], (err, results, fields) => {
           if (err) {
             debug('An error occurred while adding a Bank account', err);
             res.status(500).json({ message: 'Some error occurred' });
           }
-          if (result.length === 0) {
+          if (results.length === 0) {
             if (accountId === '' || accountName === '') {
               res.status(401).json({ message: 'Please fill in the blank spaces of the parameters.' });
             } else {
@@ -50,30 +50,29 @@ module.exports = function router() {
       } = req.body;
       db.query('SELECT at_account_name from account_table where at_user_id = ? AND at_account_name = ? AND at_account_id = ?', [req.user.username, accountName, accountId], (err, results, fields) => {
         if (results.length !== 0) {
-          if (newAccountId !== accountId || newAccountName !== accountName) {
-            if (newAccountId !== accountId) {
-              db.query(('UPDATE account_table SET at_account_id = ? WHERE at_user_id = ? AND at_account_id = ?', [newAccountId, req.user.username, accountId], () => {
-                if (err) {
-                  debug('An error occurred while editing a Bank account', err);
-                  res.status(500).json({ message: 'Some error occurred editing a bank account' });
-                } else {
-                  debug(results);
-                  res.status(200).json({ message: 'bank account updated successfully' });
-                }
-              }));
-            }
-            if (newAccountName !== accountName) {
-              db.query(('UPDATE account_table SET at_account_name = ? WHERE at_user_id = ? AND at_account_name = ?', [newAccountName, req.user.username, accountName], () => {
-                if (err) {
-                  debug('An error occurred while editing a Bank account', err);
-                  res.status(500).json({ message: 'Some error occurred adding a bank account' });
-                } else {
-                  debug(results);
-                  res.status(200).json({ message: 'bank account updated successfully' });
-                }
-              }));
-            }
+          if (newAccountId !== accountId) {
+            db.query(('UPDATE account_table SET at_account_id = ? WHERE at_user_id = ?', [newAccountId, req.user.username], (err, results, fields) => {
+              if (err) {
+                debug('An error occurred while editing a Bank account', err);
+                res.status(500).json({ message: 'Some error occurred editing a bank account' });
+              } else {
+                debug(results);
+                res.status(200).json({ message: 'bank account updated successfully' });
+              }
+            }));
+          }
+          if (newAccountName !== accountName) {
+            db.query(('UPDATE account_table SET at_account_name = ? WHERE at_user_id = ?', [newAccountId, req.user.username], (err, results, fields) => {
+              if (err) {
+                debug('An error occurred while editing a Bank account', err);
+                res.status(500).json({ message: 'Some error occurred editing a bank account' });
+              } else {
+                debug(results);
+                res.status(200).json({ message: 'bank account updated successfully' });
+              }
+            }));
           } else if (newAccountId === '' || newAccountName === '') {
+            debug('Attempted changes can not be blank', results);
             res.status(401).json({ message: 'Attempted changes cannot be blank' });
           } else {
             res.status(401).json({ message: 'Bank account name and id are not changed' });
@@ -87,9 +86,9 @@ module.exports = function router() {
   bkAccountRouter.route('/delete_account')
     .delete((req, res) => {
       const { accountId, accountName } = req.body;
-      db.query('SELECT account_name from account_table where at_user_id = ? AND at_account_name = ? OR at_account_id = ?', [req.user.username, accountName, accountId], (err, results, fields) => {
+      db.query('SELECT account_name from account_table where at_user_id = ? AND at_account_name = ? OR at_account_id = ?;', [req.user.username, accountName, accountId], (err, results, fields) => {
         if (results.length !== 0) {
-          db.query('DELETE FROM account_table WHERE at_user_id = ? AND at_account_name = ? OR at_account_id = ?', [req.user.username, accountName, accountId], () => {
+          db.query('DELETE FROM account_table WHERE at_user_id = ? AND at_account_name = ? OR at_account_id = ?;', [req.user.username, accountName, accountId], (err, results, fields) => {
             if (err) {
               debug('An error occurred while deleting a bank account', err);
               res.status(500).json({ message: 'Some error occurred deleting a bank account' });
