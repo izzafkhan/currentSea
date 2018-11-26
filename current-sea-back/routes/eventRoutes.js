07a6bdbe95fab3de2b459272d693a7cf9935bc8f
Aveek Biswas <biswasaveek@gmail.com>
2018-11-25 16:20:26 -0800

    Fixing events route

--- current-sea-back/routes/eventRoutes.js
+++ current-sea-back/routes/eventRoutes.js
@@ -5,34 +5,29 @@ const db = require('./db');
 
 module.exports = function router() {
 
-    eventRouter.use((err, req, res, next) => {
-        if (!req.user) {
-            res.status(401).json({message: 'User not logged in'});
-        }
-        next();
-    });
-
     eventRouter.route('/create_event')
         .post((req, res) => {
             const { eventAbv, transactionId, eventName } = req.body;
-            db.query('INSERT INTO event_table (et_event_abv, et_transaction_id, et_event_name) VALUES (?, ?, ?);', [eventAbv, transactionId, eventName], (err,results) => {
+            db.query('INSERT INTO event_table (et_event_abv, et_transaction_id, et_event_name, et_user_id) VALUES (?, ?, ?, ?);', [eventAbv, transactionId, eventName, req.user.username], (err,results) => {
                 if (err) {
                     debug('Error occurred while inserting to event_table in create_event route' + err);
                     res.status(500).json({message: 'Some error occurred'});
+                } else {
+                    res.status(201).json({message: 'Event created'});
                 }
-                res.status(201).json({message: 'Event created'});
             });
         });
 
     eventRouter.route('/edit_event')
         .post((req, res) => {
             const { eventId, eventAbv, transactionId, eventName } = req.body;
-            db.query('UPDATE event_table SET et_event_abv = ?, et_transaction_id = ?, et_event_name = ?) WHERE et_event_id = ?;', [eventAbv, transactionId, eventName, eventId], (err,results) => {
+            db.query('UPDATE event_table SET et_event_abv = ?, et_transaction_id = ?, et_event_name = ? WHERE et_event_id = ? and et_user_id=?;', [eventAbv, transactionId, eventName, eventId, req.user.username], (err,results) => {
                 if (err) {
                     debug('Error occurred while updating event_table in edit_event route' + err);
                     res.status(500).json({message: 'Some error occurred'});
+                } else {
+                    res.status(201).json({message: 'Event updated'});
                 }
-                res.status(201).json({message: 'Event updated'});
             });
         });
 
@@ -40,22 +35,23 @@ module.exports = function router() {
         .post((req, res) => {
             const { eventId } = req.body;
             db.query('SELECT et_event_id from event_table where et_event_id = ?', [eventId], (err, result) => {
-                if (result.length === 0) {
+                if (result== 'undefined' || result == null ||  result.length === 0) {
                     res.status(400).json({ message: 'Event id does not exist.' });
                 } else {
-                    db.query('DELETE FROM et_event_table WHERE et_event_id = ?;', [eventId], (err, results) => {
+                    db.query('DELETE FROM event_table WHERE et_event_id = ?;', [eventId], (err, results) => {
                         if (err) {
                             debug('Error occurred while deleting event in delete_event route' + err);
                             res.status(500).json({message: 'Some error occurred'});
+                        } else {
+                            res.status(200).json({message: 'Event deleted successfully'});
                         }
-                        res.status(200).json({message: 'Event deleted'});
                     });
                 }
             });
         });
 
     eventRouter.route('/archive_event')
-        .get((req, res) => {
+        .post((req, res) => {
             // Render respective html, ejs, or pug
         });
     return eventRouter;
