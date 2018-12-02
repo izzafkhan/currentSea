84e485f675659ff1b329ba22b1c1056e25de8d4f
edanyachdav <edan.yachdav@gmail.com>
2018-12-02 10:46:49 -0800

    Login and register page design - added user name to register page - Income statement page almost done, just needs to get data from backend

--- current-sea-front/src/Login/LoginForm.js
+++ current-sea-front/src/Login/LoginForm.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './LoginForm.css';
 import {Link, Redirect} from "react-router-dom";
 import $ from 'jquery';
+import Transaction from "../Reports/Transaction";
 
 export default class LoginForm extends React.Component {
 
@@ -12,7 +13,9 @@ export default class LoginForm extends React.Component {
             email: "",
             password: "",
             toRegister: false,
-            loginSuccess: false
+            loginSuccess: false,
+
+            loginForDemo: false
         };
 
     }
@@ -57,8 +60,6 @@ export default class LoginForm extends React.Component {
             pass: this.state.password
         }
 
-        console.log(loginData)
-
         $.ajax({
                 url: "http://localhost:4000/profile/login",
                 type: "POST",
@@ -67,46 +68,101 @@ export default class LoginForm extends React.Component {
                 dataType: 'json',
                 xhrFields: {withCredentials: true},
                 data: JSON.stringify(loginData),
-                success: (data) => {
+                success: (recivedData) => {
                     this.setState({loginSuccess: true});
-                    alert('Successful Login')
+                    console.log('Successful Login')
                 },
                 error: (data) => {
-                    this.setState({password: ''});
                     alert('Invalid credentials');
                 }
             }
         );
     }
 
+    onSubmitDemo = () => {
+        this.setState({loginForDemo: true})
+    }
+
+    redirectDemo = () => {
+
+        if (this.state.loginForDemo == true) {
+            console.log("redirect demo")
+            return <Transaction/>
+        }
+    }
+
     render() {
+
+
         return (
-            <div class="rootContainer">
-                <div class="container">
-                    <div class="titleLabel">
-                        <label>CurrentSea</label>
+
+            <div className="loginRoot">
+
+                <div className="navbarLoginDiv">
+                    <nav className="navbarLogin"></nav>
+                </div>
+
+
+
+                <div className="loginBody">
+
+
+                    <div className="navbarLoginFormGap">
+
                     </div>
-                    <form class="userInfoForm">
-                        <fieldset>
-                            <input type="text" className="emailField" placeholder="Email" value={this.state.value}
-                                   onChange={this.handleEmailChange}/>
-                        </fieldset>
-                        <fieldset>
-                            <input class="passwordField" type="password" placeholder="Password"
-                                   value={this.state.value} onChange={this.handlePasswordChange}/>
-                        </fieldset>
-                    </form>
-                    <div class="loginButtonSignInDiv">
-                        <button class="loginButtonSignIn" onClick={e => this.onSubmit(e)}>Login</button>
+
+                    <div className="loginContainerGrid">
+                        <div className="lcgTop">CurrentSea</div>
+
+                        <div className="lcgMiddle">
+
+
+                            <div className="loginFieldsContainer">
+                                <div>
+                                    <input type="text" className="loginFormField" placeholder="Username or Email" value={this.state.value}
+                                           onChange={this.handleEmailChange}/>
+                                </div>
+
+                                <div className="loginFieldGap">
+
+                                </div>
+
+                                <div>
+                                    <input type="password" className="loginFormField" placeholder="Password"  value={this.state.value}
+                                           onChange={this.handlePasswordChange}/>
+                                </div>
+                            </div>
+
+
+
+
+                        </div>
+
+                        <div className="lcgBottom">
+                            <div className="loginButtonContainer">
+                                <button className="loginButton" onClick={this.onSubmitDemo}>Log In</button>
+                            </div>
+
+                            <div className="registerDiv">
+                                <Link class="regLink" to="/Register">Don't have an account? Sign Up</Link>
+                            </div>
+                        </div>
+
+
+
                     </div>
-                    <div class="registerDiv">
-                        <Link class="regLink" to="/Register">Not registered? Click here.</Link>
-                    </div>
-                    <div class="forgotDiv">
-                        <Link class="forgotLink" to="/">Forgot password?</Link>
-                    </div>
+
+                    {/*<div className="loginFormContainer">*/}
+
+                        {/*<div className="loginLogo">*/}
+                            {/*<label>CurrentSea</label>*/}
+                        {/*</div>*/}
+                    {/*</div>*/}
+
                 </div>
             </div>
+
+
         );
     }
 }
--- current-sea-front/src/Reports/Tables/IncomeStatement.js
+++ current-sea-front/src/Reports/Tables/IncomeStatement.js
@@ -1,58 +1,227 @@
 import React, {Component} from 'react';
-import './IncomeStatement.css';
+import {Dropdown, DropdownToggle, DropdownMenu, DropdownItem} from 'reactstrap';
 import ReactTable from 'react-table';
+import DatePicker from "react-datepicker";
+import {Redirect} from "react-router-dom";
+import moment from "moment"
+import $ from 'jquery';
 import 'react-table/react-table.css'
+import "react-datepicker/dist/react-datepicker.css";
+import './IncomeStatement.css';
+import {Link} from "react-router-dom";
 
-class IncomeStatement extends Component{
-    constructor(props){
+export default class IncomeStatement extends Component {
+    constructor(props) {
         super(props);
 
+        this.state = {
+            data: [],
+            startDate: '',
+            endDate: '',
+            reportDropdownOpen: false,
+            currencyDropdownOpen: false,
+            gotoHome: false,
+            demoExchangeRate: '1',
+            demoDefaultCurrencyCode: 'USD'
+
+        };
+
     }
-    render(){
-        var { data = [] } = this.props
-        var columns = [{
-            Header: '#',
-            accessor: 'number'
+
+    fetchData(state, instance) {
+        $.ajax({
+                url: "http://localhost:4000/statement",
+                type: "GET",
+                contentType: "application/json; charset=utf-8",
+                crossDomain: true,
+                dataType: 'json',
+                xhrFields: {withCredentials: true},
+                data: 'userId=test',
+                success: function (receivedData) {
+                    this.setState({data: receivedData});
+                }.bind(this),
+                error: (receivedData) => {
+                    alert('error occurred')
+
+                }
+            }
+        );
+    }
+
+
+    handleChangeStart = (date) => {
+        this.setState({
+            startDate: date
+        });
+    }
+
+    handleChangeEnd = (date) => {
+        this.setState({
+            endDate: date
+        });
+    }
+
+    currencyChangedDemo = event => {
+        const currencyCode = event.target.value
+
+        if (currencyCode == "USD") {
+            this.setState({demoExchangeRate: "1"})
+            this.setState({demoDefaultCurrencyCode: 'USD'})
+        }
+
+        if (currencyCode == "GBP") {
+            this.setState({demoExchangeRate: ".78"})
+            this.setState({demoDefaultCurrencyCode: 'GBP'})
+        }
+
+        if (currencyCode == "EURO") {
+            this.setState({demoExchangeRate: ".88"})
+            this.setState({demoDefaultCurrencyCode: 'EURO'})
+        }
+    }
+
+    currencyChanged = event => {
+        let currencyCode = event.target.value
+
+        $.ajax({
+                url: "http://localhost:4000/currencies/getrate",
+                type: "Get",
+                contentType: "application/json; charset=utf-8",
+                crossDomain: true,
+                dataType: 'json',
+                xhrFields: {withCredentials: true},
+                data: {currencyFrom: 'USD', currencyTo: 'USD', date: '2018-06-06'},
+                success: function (receivedData) {
+                    console.log("Successful Get")
+                    console.log(receivedData)
+                    this.setState({data: receivedData});
+                }.bind(this),
+                error: (receivedData) => {
+                    alert('error occurred')
+
+                }
+            }
+        );
+    }
+
+    toggleReport = () => {
+        this.setState(prevState => ({
+            reportDropdownOpen: !prevState.reportDropdownOpen
+        }));
+    }
+
+    toggleCurrency = () => {
+        this.setState(prevState => ({
+            currencyDropdownOpen: !prevState.currencyDropdownOpen
+        }));
+    }
+
+
+    render() {
+
+
+
+        var data = [{
+            number: '3500', currency: 'USD', account: 'Food', change: 170*this.state.demoExchangeRate
         }, {
-            Header: 'Category',
-            accessor: 'category'
+            number: '5000', currency: 'USD', account: 'Apartment Rent', change: 25*this.state.demoExchangeRate
         }, {
-            Header: 'Account',
-            accessor: 'account'
-        }, {
-            Header: 'Change',
-            accessor: 'change'
-        }]
-        return(
-            <div className="IncomeStatement-table">
-                <ReactTable
-                        data = {data}
-                        noDataText="Your income and expenses will appear here"
-                        columns = {columns}
-                    />
+            number: '4100', currency: 'USD', account: 'Travelling', change: 2000*this.state.demoExchangeRate
+        }];
+        var columns = [
+            {
+                Header: '#',
+                accessor: 'number',
+
+            },
+            {
+                Header: 'Account',
+                accessor: 'account', width: 300
+            },
+            {
+                Header: 'Change',
+                accessor: 'change'
+            }]
+
+        return (
+            <body className="incomeStatementRoot">
+
+            <div>
+                <nav className="navbarStatements">
+                    <label class="homeLabel" >CurrentSea</label>
+                </nav>
             </div>
+
+            <div className="isReportsTitle">Reports</div>
+
+            <div className="gridContainer">
+
+                <div className="isgTop">
+
+
+                    <Dropdown className="isDropDownReports" isOpen={this.state.reportDropdownOpen}
+                              toggle={this.toggleReport}>
+                        <DropdownToggle caret>
+                            Income Statement
+                        </DropdownToggle>
+                        <DropdownMenu className="isDropDownReportsMenu" >
+                            <DropdownItem>Balance Sheet</DropdownItem>
+                        </DropdownMenu>
+                    </Dropdown>
+
+
+                    <DatePicker className="isStartDatePicker"
+                                selected={this.state.startDate}
+                                onChange={this.handleChangeStart}
+                                placeholderText="Start Date"
+                    />
+
+                    <DatePicker className="isEndDatePicker"
+                                selected={this.state.endDate}
+                                onChange={this.handleChangeEnd}
+                                minDate={this.state.startDate}
+                                placeholderText="End Date"
+                    />
+
+
+                    <Dropdown className="isDropDownCurrency"  isOpen={this.state.currencyDropdownOpen}
+                              toggle={this.toggleCurrency}>
+                        <DropdownToggle caret>
+                            {this.state.demoDefaultCurrencyCode}
+                        </DropdownToggle>
+
+                        <DropdownMenu className="isDropDownCurrencyMenu">
+                            <DropdownItem>
+                                <option onClick={this.currencyChangedDemo}>USD</option>
+                            </DropdownItem>
+                            <DropdownItem>
+                                <option onClick={this.currencyChangedDemo}>GBP</option>
+                            </DropdownItem>
+                            <DropdownItem>
+                                <option onClick={this.currencyChangedDemo}>EURO</option>
+                            </DropdownItem>
+                        </DropdownMenu>
+
+                    </Dropdown>
+
+
+                </div>
+
+                <div className="isgBottom">
+                    <ReactTable
+                        className="isDataTable"
+                        data={data}
+                        noDataText="Your income and expenses will appear here"
+                        columns={columns}
+                    />
+
+                </div>
+
+
+            </div>
+
+            </body>
+
         );
     }
 }
-
-export default IncomeStatement
-
-{/*}
-            <table className="IncomeStatement-table">
-                <thead>
-                    <tr>
-                        <th id="header-number">
-                            <div className="IncomeStatement-number">#</div>
-                        </th>
-                        <th id="header-category">
-                            <div className="IncomeStatement-category"> </div>
-                        </th>
-                        <th id="header-account">
-                            <div className="IncomeStatement-account">Account</div>
-                        </th>
-                        <th id="header-change">
-                            <div className="IncomeStatement-change">Change</div>
-                        </th>
-                    </tr>
-                </thead>
-            </table>*/}
