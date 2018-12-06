79d6f8b0c64ec025e54a22554b06b45b72f78c0c
Alexander Behrmann <alexander.behrmann@hotmail.com>
2018-12-05 21:01:48 -0800

    dropdowns in transactions implemented

--- current-sea-front/src/Reports/Report.js
+++ current-sea-front/src/Reports/Report.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
 import './Report.css';
-import Menu from './Menu'
-import BalanceSheet from './Tables/BalanceSheet'
-import IncomeStatement from './Tables/IncomeStatement'
-import Header from '../Header'
+import Menu from './Menu';
+import BalanceSheet from './Tables/BalanceSheet';
+import IncomeStatement from './Tables/IncomeStatement';
+import Header from '../Header';
+import $ from 'jquery';
 import {Dropdown, DropdownToggle, DropdownMenu, DropdownItem} from 'reactstrap';
 
 class Report extends Component {
@@ -11,12 +12,13 @@ class Report extends Component {
         super(props);
 
         this.state = {
-            presentIncomeStatement: false
-            
+            presentIncomeStatement: false,
+            currencies: []
         }
 
         this.handleIncomeStatementTapped = this.handleIncomeStatementTapped.bind(this);
-        this.handleBalanceSheetTapped = this.handleBalanceSheetTapped.bind(this)
+        this.handleBalanceSheetTapped = this.handleBalanceSheetTapped.bind(this);
+        this.componentDidMount = this.componentDidMount.bind(this);
     }
 
     handleIncomeStatementTapped() {
@@ -37,7 +39,29 @@ class Report extends Component {
 
         console.log("in balance")
     }
-
+    componentDidMount() {
+        $.ajax({
+            url: "http://localhost:4000/currencies/currencies",
+           type: "GET",
+           contentType: "application/json; charset=utf-8",
+           crossDomain: true,
+           dataType:"json",
+           xhrFields: { withCredentials:true },
+           success: (data) => {
+               let currencies = []
+                for (let i = 0; i < data.currencies.length; i++) {
+                    const newRow = {value: '', label: ''};
+                    newRow.value = data.currencies[i];
+                    newRow.label = data.currencies[i];
+                    currencies[i] = newRow;
+                }
+                this.setState({currencies: currencies });
+           },
+           error: () => {
+                console.log("Error: Could not fetch data");
+           }
+        });
+    }
     //
     //
     // showBalanceSheet(event){
@@ -56,10 +80,10 @@ class Report extends Component {
 
         const currentSheet = () => {
             if (this.state.presentIncomeStatement == true) {
-                return <IncomeStatement action={this.handleIncomeStatementTapped}/>
+                return <IncomeStatement action={this.handleIncomeStatementTapped} currencies={this.state.currencies}/>
 
             }
-            return <BalanceSheet action={this.handleBalanceSheetTapped}/>
+            return <BalanceSheet action={this.handleBalanceSheetTapped} currencies={this.state.currencies}/>
         }
 
         return(
