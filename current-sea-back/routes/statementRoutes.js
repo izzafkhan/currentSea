3e83ecf46b7cf2956702ce9b0393db68b71bf876
Alexander Behrmann <alexander.behrmann@hotmail.com>
2018-12-06 17:04:55 -0800

    income statement is displayed

--- current-sea-back/routes/statementRoutes.js
+++ current-sea-back/routes/statementRoutes.js
@@ -98,10 +98,10 @@ module.exports = function router() {
   statementRouter.route('/statement')
     .get((req, res) => {
       if (req.user) {
-        db.query('SELECT * FROM details_table, transaction_table, account_table '
+        db.query('SELECT * FROM details_table, transaction_table, account_table, currency_table '
           + 'WHERE at_user_id = dt_userID AND at_account_id = dt_accountID '
           + 'AND tt_user_id = dt_userID AND tt_transaction_id = dt_transactionID AND dt_userID = ? '
-          + 'AND account_type = ? ORDER BY dt_accountID, tt_date ASC, dt_transactionID;',
+          + 'AND account_type = ? AND tt_currency = ct_from AND ct_to = "USD" AND ct_date = tt_date ORDER BY dt_accountID, tt_date ASC, dt_transactionID;',
         [req.user.username, 'Income'],
         (err, results) => {
           const end_amount = [];
@@ -118,13 +118,13 @@ module.exports = function router() {
               accountName.push(results[i].at_account_name);
               // credit not null for first transaction
               if (results[i].dt_credit) {
-                end -= results[i].dt_credit;
-                start = -results[i].dt_credit;
+                end -= results[i].dt_credit * results[i].ct_rate;
+                start = -results[i].dt_credit * results[i].ct_rate;
               }
               // debit not null for first transaction
               else if (results[i].dt_debit) {
-                end += results[i].dt_debit;
-                start = results[i].dt_debit;
+                end += results[i].dt_debit * results[i].ct_rate;
+                start = results[i].dt_debit * results[i].ct_rate;
               }
               if (i === results.length - 1) {
                 if (end < 0) {end_amount.push(math.abs(end));}
@@ -133,8 +133,8 @@ module.exports = function router() {
               }
               // loop through same account_id
               while (results[i].dt_accountID === results[i + 1].dt_accountID) {
-                if (results[i + 1].dt_credit) {end -= results[i + 1].dt_credit;}
-                else if (results[i + 1].dt_debit) {end += results[i + 1].dt_debit;}
+                if (results[i + 1].dt_credit) {end -= results[i + 1].dt_credit * results[i+1].ct_rate;}
+                else if (results[i + 1].dt_debit) {end += results[i + 1].dt_debit * results[i+1].ct_rate;}
                 i++;
                 if (i >= results.length - 1) {break;}
               }
