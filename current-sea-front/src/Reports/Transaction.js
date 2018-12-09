85772a99da65462ab4435b531bfde19e27ff6f04
Danny <dannyfung97@gmail.com>
2018-12-08 21:56:04 -0800

    Added subtitles

--- current-sea-front/src/Reports/Transaction.js
+++ current-sea-front/src/Reports/Transaction.js
@@ -447,7 +447,9 @@ export default class Transaction extends React.Component {
         return (
             <div class="bigContainer">
                 <h1 id='h1title'>Bookkeeping</h1>
-                <div class="myContainer">
+                <div className="settingSubHead">Here you can create records of your financial transactions</div>
+
+                <div class="myContainer" style={{marginTop: "30px"}}>
                     <div className="transaction-table">
                         <table id='dataTable' width="600">
                             <thead>
