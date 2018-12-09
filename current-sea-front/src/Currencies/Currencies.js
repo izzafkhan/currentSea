0b75f1d061d761553e9501b6f449b8d31f219a87
Danny <dannyfung97@gmail.com>
2018-12-08 18:56:16 -0800

    Updated pagination

--- current-sea-front/src/Currencies/Currencies.js
+++ current-sea-front/src/Currencies/Currencies.js
@@ -148,6 +148,7 @@ export default class Currencies extends React.Component {
                                         data={this.fav_currencies}
                                         noDataText="Your favorite currencies will appear here"
                                         columns={columns}
+                                        pageSizeOptions={[5, 10, 20]}
                                     />
                                 </div>
 
